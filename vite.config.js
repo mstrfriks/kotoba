a1d54b7f623ebe4d9f49f8ea14d2d874201ce9e8
mstrfriks <mstrfriks@users.noreply.github.com>
2026-04-20 13:23:08 +0200

    Publish site from docs

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/kotoba/",
+  base: "/kotoba/docs/",
   plugins: [react(), tailwindcss()],
 });
